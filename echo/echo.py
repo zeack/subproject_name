3dd29aea98971561fc4409ab4ae912e8ed6b18e0
Diego <dlicea@covalto.com>
2023-02-07 10:39:03 -0600

    feat: add echo function

---
+++ echo/echo.py
@@ -0,0 +1,5 @@
+def echo(phrase: str) -> None:
+   """A dummy wrapper around print."""
+   # for demonstration purposes, you can imagine that there is some
+   # valuable and reusable logic inside this function
+   print(phrase)
