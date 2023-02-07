2bbc496117c851917b77cf246be43002bffb96ff
Diego <dlicea@covalto.com>
2023-02-07 10:48:10 -0600

    feat: add hello

--- echo/echo.py
+++ echo/echo.py
@@ -3,3 +3,6 @@ def echo(phrase: str) -> None:
    # for demonstration purposes, you can imagine that there is some
    # valuable and reusable logic inside this function
    print(phrase)
+
+def hello():
+   print("hello world")
